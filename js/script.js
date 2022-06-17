5bb05585154f012093454cafb7fe6989a33a3ac0
Genalaroa <genalaroa88@gmail.com>
2022-06-17 18:37:34 +0200

    2DA PARTE

--- js/script.js
+++ js/script.js
@@ -9,43 +9,110 @@ alert ('Bienvenid@ a priedra, papel o tijera  ' +nombre+ '  te deseo mucha suert
 const roca_div = document.getElementById ("piedra");
 const papper_div = document.getElementById ("papel");
 const tije_div = document.getElementById ("tijera");
+let marcadorUsuario = 0;
+let marcadoBot = 0;
+const marcadorUsuario_span = document.getElementById("marcadorUsuario");
+const marcadorBot_span = document.getElementById("marcadorBot");
+const eleccion = ['piedra', 'papel', 'tijera'];
+
+
 
   function boteleccion () { 
-
-    const eleccion = ['piedra', 'papel', 'tijera'];
     const random = Math.floor(Math.random () *3);
     return eleccion [random];
     
   }
 
 
-  function jugar (eleccionUsuario){ 
-
-    const eleccionBot = boteleccion ();
-    console.log (nombre+ '  a escogido  ' +eleccionUsuario);
-    console.log ('El bot a escogido  ' +eleccionBot);
-  }
-
-
-
   function opciones() { 
 
-  
     roca_div.addEventListener('click', function(){
       jugar("piedra");
     });
 
     papper_div.addEventListener('click', function() {
-      jugar("papel");
+      jugar("papel");    
     });
 
     tije_div.addEventListener('click', function() {
       jugar("tijera");
     });
 
-  }
+    }
 
   opciones();
+
+
+
+  function jugar (usuariochoose){ 
+
+    const eleccionBot = boteleccion ();
+
+    if (usuariochoose=="piedra") {
+
+          if (eleccionBot=="tijera") {
+            alert(usuariochoose+" gana a "+eleccionBot+" Tu ganas!");
+            marcadorUsuario++;
+          
+          }
+          if (eleccionBot=="papel") {
+          
+            alert(eleccionBot+" gana a "+usuariochoose+" Perdiste!");
+            marcadoBot++;
+          }
+          if (eleccionBot=="piedra") {
+            
+            alert(usuariochoose+" empata con "+eleccionBot+" Empate!");
+          }
+          
+    }
+
+
+    if (usuariochoose=="papel") {
+
+      if (eleccionBot=="tijera") {
+        
+        alert(eleccionBot+" gana a "+usuariochoose+" Perdiste!");
+        marcadoBot++;
+      }
+      if (eleccionBot=="papel") {
+      
+        alert(usuariochoose+" empata con "+eleccionBot+" Empate!");
+      }
+      if (eleccionBot=="piedra") {
+      
+        alert(usuariochoose+" gana a "+eleccionBot+" Tu ganas!");
+        marcadorUsuario++;
+      }
+      
+
+}
+
+if (usuariochoose=="tijera") {
+
+  if (eleccionBot=="tijera") {
+    
+    alert(usuariochoose+" empata con "+eleccionBot+" Empate!");
+  }
+  if (eleccionBot=="papel") {
+    
+    alert(usuariochoose+" gana a "+eleccionBot+" Tu ganas!");
+    marcadorUsuario++;
+  }
+  if (eleccionBot=="piedra") {
+  
+    alert(eleccionBot+" gana a "+usuariochoose+" Perdiste!");
+    marcadoBot++;
+  }
+}
+  
+
+          marcadorUsuario_span.innerHTML=marcadorUsuario;
+          marcadorBot_span.innerHTML=marcadoBot;
+          
+      console.log (usuariochoose +"||"+  eleccionBot );
+   // 
+  }
 
 
 
